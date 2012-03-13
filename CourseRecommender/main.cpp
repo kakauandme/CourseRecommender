e286cb896026e7a716960783520a8e1857683af4
Kirill Kliavin <k.kliavin@gmail.com>
2012-03-13 16:45:06 +1100

    Initial Commit

---
+++ CourseRecommender/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CourseRecommender
+//
+//  Created by Kirill Kliavin on 13/03/12.
+//  Copyright (c) 2012 RMIT. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
